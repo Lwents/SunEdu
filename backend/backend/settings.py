b5a76944d2914b1aead5dc31f3d6c65898ac71df
Playboyvn777 <kiritovn777@gmail.com>
2025-11-11 10:19:03 +0700

    fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -123,9 +123,9 @@ TEMPLATES = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.getenv("DB_NAME", ""),
-        "USER": os.getenv("DB_USER", ""),
-        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "NAME": os.getenv("DB_NAME", "elearing"),
+        "USER": os.getenv("DB_USER", "elearing"),
+        "PASSWORD": os.getenv("DB_PASSWORD", "123456"),
         "HOST": os.getenv("DB_HOST", "127.0.0.1"),
         "PORT": os.getenv("DB_PORT", "5432"),
     }
